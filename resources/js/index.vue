34bbfca6c3db0c47c50dd070c8b2459e6f3c83e2
Romel Niño Paano <rpaano02@gmail.com>
2020-05-15 16:33:08 +0800

    Add nav to index vue

--- resources/js/index.vue
+++ resources/js/index.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
-        <router-link to="/">Home</router-link>
-        <router-link to="/second">Second</router-link>
-        <router-view/>
+        <nav class="navbar bg-white border-bottom navbar-light">
+            <router-link class="navbar-brand mr-auto" :to="{name: 'home'}">LaravelBnb</router-link>
+            <router-link class="btn nav-btn" :to="{name: 'second'}">Second</router-link>
+        </nav>
+
+        <div class="container my-4 px-4">
+            <router-view/>
+        </div>
+
     </div>
 </template>
