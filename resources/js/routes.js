cd1ad4def1851881ba016c9be6415b83f356492d
Romel Niño Paano <rpaano02@gmail.com>
2020-05-15 15:08:57 +0800

    Add Exmaple2 to Vue Route

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,11 +1,17 @@
 import VueRouter from "vue-router";
 import ExampleComponent from "./components/ExampleComponent";
+import Example2 from "./components/Example2";
 
 const routes = [
     {
         path: "/",
         component: ExampleComponent,
         name: "home",
+    },
+    {
+        path: "/second",
+        component: Example2,
+        name: "second",
     }
 ];
 
