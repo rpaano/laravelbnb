5757e60b79ab03d666fda73b982ecef7c4597f72
Romel Niño Paano <rpaano02@gmail.com>
2020-05-15 21:44:29 +0800

    Add Bookables to / route

--- resources/js/bookables/BookableListItem.vue
+++ resources/js/bookables/BookableListItem.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
-        
+        <h1>Title</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, placeat!</p>
     </div>
 </template>
--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -1,5 +1,17 @@
 <template>
     <div>
-
+        <bookable-list-item/>
+        <bookable-list-item/>
+        <bookable-list-item/>
+        <bookable-list-item/>
     </div>
 </template>
+
+<script>
+    import BookableListItem from "./BookableListItem";
+    export default {
+        components: {
+            BookableListItem
+        }
+    }
+</script>
