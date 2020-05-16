83b30f72c056390523ccfeb43dc6e6521ab403e0
Romel Niño Paano <rpaano02@gmail.com>
2020-05-16 15:31:42 +0800

    Add update after 5 seconds

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <bookable-list-item item-title="Cheap Villa" item-content="A very cheap villa" :price="1000"/>
-        <bookable-list-item item-title="Cheap Villa2" item-content="A very cheap villa2" :price="1500"/>
+        <bookable-list-item :item-title="bookable1.title" :item-content="bookable1.content" :price="1000"/>
+        <bookable-list-item :item-title="bookable2.title" :item-content="bookable2.content" :price="1500"/>
     </div>
 </template>
 
@@ -11,11 +11,30 @@
         components: {
             BookableListItem
         },
+        data() {
+            return {
+                bookable1: {
+                    title: "Cheap Villa",
+                    content: "A very cheap villa",
+                },
+                bookable2: {
+                    title: "Cheap Villa 2",
+                    content: "A very cheap villa 2",
+                },
+            }
+        },
         // beforeCreate() {
         //     console.log('before create');
         // },
         created() {
             console.log('create');
+            console.log(this.bookable1);
+            console.log(this.bookable2);
+
+            setTimeout(() => {
+                this.bookable1.title = "Not cheap villa2";
+                this.bookable2.title = "Not so cheap villa";
+            }, 5000)
         },
         // beforeMount() {
         //     console.log('before mount');
