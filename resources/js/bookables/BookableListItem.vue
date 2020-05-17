76dbc5480ed06112395192fb10396a76ba7a347f
Romel Niño Paano <rpaano02@gmail.com>
2020-05-18 01:00:30 +0800

    Update the bootstrap to align properly

--- resources/js/bookables/BookableListItem.vue
+++ resources/js/bookables/BookableListItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card">
+    <div class="card w-100">
         <div class="card-body">
             <div class="card-title">
                 <h5>{{ itemTitle }}</h5>
--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -6,7 +6,7 @@
         <div v-else>
             <div class="row mb-4" v-for="row in rows" :key="'row' + row">
                 <div
-                    class="col"
+                    class="col d-flex align-items-lg-stretch"
                     v-for="(bookable, column) in bookablesInRow(row)"
                     :key="'row' + row + column"
                 >
