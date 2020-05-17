3967b56e8e8799207e976dd1d07b67376622cbf3
Romel Niño Paano <rpaano02@gmail.com>
2020-05-17 17:50:32 +0800

    Create BookableFactory.php

---
+++ database/factories/BookableFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Bookable;
+use Faker\Generator as Faker;
+
+$factory->define(Bookable::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
