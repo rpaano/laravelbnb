15781763f8d9983e1a7865f031d81b4463c2aa87
Romel Niño Paano <rpaano02@gmail.com>
2020-05-18 12:35:56 +0800

    Add new route bookable

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,12 +1,17 @@
 import VueRouter from "vue-router";
 import Bookables from "./bookables/Bookables";
+import Bookable from "./Bookable/Bookable";
 
 const routes = [
     {
         path: "/",
         component: Bookables,
         name: "home",
-    },
+    },{
+        path: "/bookable/:id",
+        component: Bookable,
+        name: "bookable",
+    }
 ];
 
 const router = new VueRouter({
