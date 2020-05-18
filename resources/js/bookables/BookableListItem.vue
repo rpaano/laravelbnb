eaa894a9d81c5223fc1a4bcfe828a25950f699d7
Romel Niño Paano <rpaano02@gmail.com>
2020-05-18 14:01:25 +0800

    Add link to the cards

--- resources/js/bookables/BookableListItem.vue
+++ resources/js/bookables/BookableListItem.vue
@@ -2,7 +2,9 @@
     <div class="card w-100">
         <div class="card-body">
             <div class="card-title">
-                <h5>{{ title }}</h5>
+                <router-link :to="{ name: 'bookable', params: { id } }">
+                    <h5 class="card-title">{{ title }}</h5>
+                </router-link>
                 <div class="card-text">
                     <p>{{ description }}</p>
                 </div>
