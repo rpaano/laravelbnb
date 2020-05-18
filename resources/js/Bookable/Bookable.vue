1772178c4b1f823bb30bc405763f7860e6908fe4
Romel Niño Paano <rpaano02@gmail.com>
2020-05-18 15:01:06 +0800

    Add loading and response to the api

--- resources/js/Bookable/Bookable.vue
+++ resources/js/Bookable/Bookable.vue
@@ -1,6 +1,22 @@
 <template>
-    <div>
-        you
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <div v-if="!loading">
+                        <h2>{{ bookable.title }}</h2>
+                        <hr/>
+                        <article>{{ bookable.description }}</article>
+                    </div>
+                    <div v-else>
+                        Loading...
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            availability & prices
+        </div>
     </div>
 </template>
 
@@ -10,12 +26,17 @@
         data() {
             return {
                 bookable: null,
+                loading: null,
             }
         },
 
         created() {
+            this.loading = true;
             axios.get(`/api/bookables/${this.$route.params.id}`)
-                .then(response => this.bookable = response.data);
+                .then(response => {
+                    this.bookable = response.data;
+                    this.loading = false;
+                });
         },
     }
 </script>
